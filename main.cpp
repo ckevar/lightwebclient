d2ba1faed3438e6aeb4063f3666740180a037526
Christian Alvarado <ckevar@pop-os.localdomain>
2023-06-22 18:59:50 +0200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
 	// webClient.set_header("Authorization: bxase\r\n", 22);
 	// webClient.set_header("Authorization-2: bxase2\r\n", 25);
 
-	webClient.get("/bot6234049897:AAGWLi4-5vCQwQhraFUBgH-me3970XO1byg/getMe", 56, buff, buff_size);
+	webClient.get("/bot<BOT-TOKEN>/getMe", 56, buff, buff_size);
 	// webClient.post(resource, content, content_length);
 	
 	//webClient.free() /* No needed the WebClientSSL destructor */
